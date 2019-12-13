5a268565a201ff1759daae8938c18b3abe7de807
Andreas Ny <andreas.ny@chasacademy.se>
2019-12-13 16:59:32 +0100

    Add index.php Create 'Customers' and 'Cars' table

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+    echo "<h1> Wanna rent a car?</h1>";
+
--- sql/database.sql
+++ sql/database.sql
@@ -1,1 +1,19 @@
-use carRental;
+DROP DATABASE IF EXISTS carRental;
+CREATE DATABASE carRental;
+USE carRental;
+
+
+CREATE TABLE Customers (socialSecurityNumber INTEGER NOT NULL KEY,
+                        customerName VARCHAR(256), address VARCHAR(256),
+                        postalAddress VARCHAR(256), phoneNumber INTEGER
+                       );
+
+CREATE TABLE Cars (registration INTEGER NOT NULL KEY,
+                    make VARCHAR(256),
+                    color VARCHAR(256),
+                    year INTEGER,
+                    cost INTEGER,
+                    renter INTEGER NOT NULL,
+                    FOREIGN KEY (renter) REFERENCES Customers(socialSecurityNumber),
+                    rentStartTime TIMESTAMP
+                  );
