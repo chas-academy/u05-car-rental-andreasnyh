c1ba43bdcb2ebd36855db4ddf639178c52036fb4
Andreas Ny <andreas.ny@chasacademy.se>
2019-12-17 14:25:13 +0100

    Add Color and Car Makes to Database

--- sql/database.sql
+++ sql/database.sql
@@ -3,18 +3,41 @@ CREATE DATABASE carRental;
 USE carRental;
 
 
-CREATE TABLE Customers (socialSecurityNumber INTEGER NOT NULL KEY,
-                        customerName VARCHAR(256), address VARCHAR(256),
-                        postalAddress VARCHAR(256), phoneNumber INTEGER
+CREATE TABLE Customers (socialSecurityNumber INTEGER NOT NULL PRIMARY KEY,
+                        customerName VARCHAR(256),
+                        address VARCHAR(256),
+                        postalAddress VARCHAR(256),
+                        phoneNumber INTEGER
                        );
 
-CREATE TABLE Cars (registration INTEGER NOT NULL KEY,
-                    make VARCHAR(256),
-                    color VARCHAR(256),
-                    year INTEGER,
-                    cost INTEGER,
+CREATE TABLE Cars (registration VARCHAR(100) NOT NULL PRIMARY KEY,
+                    year YEAR,
+                    cost FLOAT,
                     renter INTEGER NOT NULL,
                     FOREIGN KEY (renter) REFERENCES Customers(socialSecurityNumber),
-                    rentStartTime TIMESTAMP
+                    rentStartTime DATETIME
                   );
 
+CREATE TABLE Makes (make VARCHAR(256));
+
+INSERT INTO Makes (make) VALUES ("Ford"),
+                                ("Volkswagen"),
+                                ("Toyota"),
+                                ("Hyundai"),
+                                ("Checrolet"),
+                                ("Honda"),
+                                ("Kia"),
+                                ("Mazda");
+
+CREATE TABLE Colors (color VARCHAR(256));
+
+insert INTO Colors (color) values ("Black"),
+                                  ("White"),
+                                  ("Gray"),
+                                  ("Blue"),
+                                  ("Red"),
+                                  ("Silver"),
+                                  ("Orange"),
+                                  ("Green");
+
+#CREATE TABLE History ();
