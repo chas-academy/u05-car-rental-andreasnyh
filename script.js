0ffaf9f24453ab3abed9b718f2818259fb91e4f5
Andreas Ny <andreas.ny@chasacademy.se>
2020-01-14 22:15:12 +0100

    Start of styling

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 let ssn = document.querySelector("input[name='socialSecurityNumber']");
-var form = document.forms[0];
-var ssn = form.querySelector("input[name='customerName']");
-var selectedValue = selectElement.value;
-ssn.value="Finkar Detta?";
+console.log(ssn);
+ssn.setAttribute("value", "skriv här");
