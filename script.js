7d399468c22cc2b7619cc349b1f418e1d7b74c26
Andreas Ny <andreas.ny@chasacademy.se>
2020-01-14 21:29:08 +0100

    Add javascript  file

---
+++ script.js
@@ -0,0 +1,5 @@
+let ssn = document.querySelector("input[name='socialSecurityNumber']");
+var form = document.forms[0];
+var ssn = form.querySelector("input[name='customerName']");
+var selectedValue = selectElement.value;
+ssn.value="Finkar Detta?";
