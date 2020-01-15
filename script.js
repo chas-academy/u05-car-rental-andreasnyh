a2ddffdb2992aa8598ca2dac7cc2107649cdc05d
Andreas Ny <andreas.ny@chasacademy.se>
2020-01-15 19:57:42 +0100

    Underway implementing form validation on AddCustomer with JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 let ssn = document.querySelector("input[name='socialSecurityNumber']");
-var form = document.forms[0];
-var ssn = form.querySelector("input[name='customerName']");
-var selectedValue = selectElement.value;
+let submitButton = document.querySelector("input[type='submit'");
+
 ssn.value="Finkar Detta?";
+
+submitButton.addEventListener("click", function (event) {
+
+    if (validateSSN(ssn.value) == false){
+        alert("Please input a valid Swedish Social Security Number!");
+        event.preventDefault();
+    }
+}, false);
+
+function validateSSN(ssn) {
+    console.log(ssn);
+    let ssnPattern = new RegExp(/\d\d[0-1]\d[0-3]\d\d\d\d\d/);
+    console.log(ssnPattern.test(ssn));
+    if (ssnPattern.test(ssn)){
+        
+        console.log("Valid SSN" + ssn);
+        return true;
+    } else {
+        console.log("Invalid SSN!")
+        return false;
+    }
+
+}
+/*
+document.querySelector("#id-checkbox").addEventListener("click", function(event) {
+    document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+    event.preventDefault();
+}, false);*/
