8cd3c3602f4efa15f4989dee818ba28e7e7315cb
Andreas Ny <andreas.ny@chasacademy.se>
2020-01-19 17:17:57 +0100

    Add comments and Database diagram

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -14,18 +14,13 @@
     }
 
     public function route(Request $request): string {
-
+        // request == path, method(get/post), parameters/form-data
         $path = $request->getPath();
 
         foreach ($this->routeMap as $route => $info) {
             $map = [];
+            // if a parameter is not set, set it to Null
             $params = isset($info["params"]) ? $info["params"] : null;
-
-            // $route: "editCustomer/:customerNumber/:customerName"
-            // $path: /editCustomer/7/Erik%20Dumas
-            // $params: ["customerNumber" => "number", "customerName" => "string"]
-
-            // $map = ["customerNumber" => 7, "customerName" => "Erik%20Dumas" "x" => "y"]
 
             if ($this->match($route, $path, $params, $map)) {
                 $controllerName = '\Main\controllers\\' .
@@ -83,7 +78,7 @@
     private function typeMatch($value, $type)
     {
         switch ($type) {
-            case "number": // ^: början, $: slutet, +: ett eller flera, *: noll eller flera, ?, exakt ett
+            case "number":
                 return preg_match('/^[0-9]+$/', $value);
 
             case "string":
